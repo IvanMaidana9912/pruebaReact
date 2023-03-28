a0bfe970a415d3dfa1ac87bcd89caf339340c877
Ivan Maidana <107094517+Ivan9912@users.noreply.github.com>
2023-03-28 03:06:38 -0300

    Update ItemDetail.jsx

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Images from "./Image";
 import Treeid from "./Treeid";
 import ResidencialJson from "./json/residencial.json"
 
 
 const ItemDetail = ({ items }) => {
+
     return (
         <div className="card mb-3 pt-xl-5 w-100">
             <div className="row g-0 d-flex flex-column align-content-center">
@@ -13,7 +13,7 @@ const ItemDetail = ({ items }) => {
                     <div className="card-body">
                         <h5 className="card-title">{items.titulo}</h5>
                         <div className="d-flex text-center justify-content-center">
-                            <p className="card-text w-75 text-start">{items.slogan}</p>
+                            <p className="card-text w-100 text-start">{items.slogan}</p>
                         </div>
                         <ul>
                             {items.detalle && items.detalle.map(item => {
@@ -21,13 +21,10 @@ const ItemDetail = ({ items }) => {
                             })}
                         </ul>
                         <div className="d-flex justify-content-center">
-                            <Link className="card-text" src={items.catalogo} target={"_blank"}>
-                                Descargar Catalogo
-                            </Link>
+                            {
+                                items.catalogo ? <button onClick={() => {window.open(items.catalogo, '_blank')}} className="btn btn-info mt-5">Descargar Catálogo</button> : ""
+                            }
                         </div>
-                        {/* {
-                                items.catalogo ? <Link id="catalogoLinkprimary" className="card-text" src={items.catalogo}><b>Descargar Catálogo</b></Link> : ""
-                            } */}
                     </div>
                     <div className="col-md-4 mb-5 d-flex w-50 flex-column">
                         <img src={items.img[0]} className="img-fluid rounded-start mb-3" alt={items.titulo} />
