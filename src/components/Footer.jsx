5ee5b8e57f0c9a398981e3c6b4c53ce45230ccc3
Ivan Maidana <107094517+Ivan9912@users.noreply.github.com>
2023-03-28 03:06:06 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,14 +15,14 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="col d-flex justify-content-center align-items-center">
-                    <img src="../../public/img/LOGOS/01.png" width={100} height={100} alt="photo"></img>
+                    <img className="footerImg" onClick={()=> {console.log("DEJAME DE CLICKEAR!!")}} src="../../public/img/LOGOS/01.png" width={100} height={100} alt="photo"></img>
                 </div>
                 <div className="mb-4 col d-flex flex-row text-end align-items-center justify-content-end">
-                    <Link className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-whatsapp"></ion-icon></Link>
-                    <Link className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-twitter"></ion-icon></Link>
-                    <Link className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-google"></ion-icon></Link>
-                    <Link className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-instagram"></ion-icon></Link>
-                    <Link className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-linkedin"></ion-icon></Link>
+                    <Link onClick={() => {window.open("https://web.whatsapp.com/", '_blank')}} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-whatsapp"></ion-icon></Link>
+                    <Link onClick={() => {window.open("https://twitter.com/?lang=es", '_blank')}} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-twitter"></ion-icon></Link>
+                    <Link onClick={() => {window.open("https://www.google.com/", '_blank')}} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-google"></ion-icon></Link>
+                    <Link onClick={() => {window.open("https://www.instagram.com/", '_blank')}} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-instagram"></ion-icon></Link>
+                    <Link onClick={() => {window.open("https://www.linkedin.com/feed/", '_blank')}} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-linkedin"></ion-icon></Link>
                 </div>
                 <div>
                     <div>
