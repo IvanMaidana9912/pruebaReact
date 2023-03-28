ef38425b73147a079ecda0602734e19a876fe69a
Ivan Maidana <107094517+Ivan9912@users.noreply.github.com>
2023-03-28 06:46:05 +0000

    mod final por hoy

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -6,12 +6,12 @@ const Images = ({ itemImg }) => {
             <div className="col-md-4">
                 <div>
                     {
-                        itemImg.img[1] ? <img key={itemImg.img} className="img-fluid rounded-start" src={itemImg.img[1]} alt={itemImg.titulo} /> : ""
+                        itemImg.img[1] ? <img key={itemImg.img} className="img-fluid rounded-start animationImgDetail" src={itemImg.img[1]} alt={itemImg.titulo} /> : ""
                     }
                 </div>
                 <div className="col-md-4 d-flex w-100 ">
                     {
-                        itemImg.subImg ? itemImg.subImg.map(img => <img key={img}  className="img-fluid rounded-start m-2" src={img} alt={"image"} />) : ""
+                        itemImg.subImg ? itemImg.subImg.map(img => <img key={img}  className="img-fluid rounded-start m-2 animationImgDetail" src={img} alt={"image"} />) : ""
                     }
                 </div>
             </div>
