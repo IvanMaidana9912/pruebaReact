6d467b6e3193a8264e4d481cb3027dadeae7dccf
Gaston Romero <107086703+GastonRomeroo@users.noreply.github.com>
2023-03-31 23:19:21 +0000

    Mejoras en el responsive del footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,18 +7,18 @@ export const Footer = () => {
             <div>< Animation /></div>
             <div className="p-4 row pt-3 bg-dark text-center text-white">
                 <div className="col">
-                    <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 w-100 text-start p-5">
+                    <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 w-100 text-center p-5">
                         <h6 className="text-uppercase fw-bold mb-4 p-3"> <ion-icon name="snow-outline"></ion-icon>Contact Us<ion-icon name="snow-outline"></ion-icon></h6>
                         <p><i><ion-icon name="map-outline"></ion-icon> Quilmes, Buenos Aires, AR</i></p>
                         <p><i><ion-icon name="mail-outline"></ion-icon> bairesvrv@gmail.com</i></p>
                     </div>
                 </div>
-                <div className="col d-flex justify-content-center align-items-center">
+                <div className="col d-flex justify-content-center align-items-center mb-5">
                     <Link to={"/"}>
                         <img className="footerImg" onClick={() => { console.log("DEJAME DE CLICKEAR!!") }} src="https://i.imgur.com/FfB5Z41.jpg" width={100} height={100} alt="photo"></img>
                     </Link>
                 </div>
-                <div className="mb-4 col d-flex flex-row text-end align-items-center justify-content-end">
+                <div className="mb-4 col d-flex flex-row text-end align-items-center justify-content-center">
                     <Link onClick={() => { window.open("https://web.whatsapp.com/", '_blank') }} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-whatsapp"></ion-icon></Link>
                     <Link onClick={() => { window.open("https://twitter.com/?lang=es", '_blank') }} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-twitter"></ion-icon></Link>
                     <Link onClick={() => { window.open("https://www.google.com/", '_blank') }} className="btn btn-outline-light btn-floating m-1"><ion-icon name="logo-google"></ion-icon></Link>
