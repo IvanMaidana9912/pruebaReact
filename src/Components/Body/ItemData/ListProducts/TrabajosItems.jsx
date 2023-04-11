968c1b63cc8703c94ca844c044509e833c10eb78
Ivan29912 <129027689+Ivan29912@users.noreply.github.com>
2023-04-11 20:07:43 +0000

    trabajos

--- src/Components/Body/ItemData/ListProducts/TrabajosItems.jsx
+++ src/Components/Body/ItemData/ListProducts/TrabajosItems.jsx
@@ -1,13 +1,25 @@
-import fotoConstruccion from "../../../../BBDD/dataLogos.json"
+// import fotoConstruccion from "../../../../BBDD/dataLogos.json"
+import json from "../../../../BBDD/dataimgtrabajos.json"
 
-export const Trabajos = ({data}) => {
+export const Trabajos = ({ data }) => {
     const primeraLetraMayuscula = `${data[0].category[0].toUpperCase()}${data[0].category.slice(1)}`;
+    console.log(json[1].videos);
+    const images = json[0].imagenes;
 
     return (
-        <div className="TrabajosItems"> 
-            {/* className="div-construccion" */}
+        <div>
             <h1 className="text-center">{primeraLetraMayuscula} realizados por BAVRV</h1>
-            <img src={fotoConstruccion[0].construccion} alt="construccion" width={"1000px"}></img>
+            <div className="d-flex flex-wrap mt-4 justify-content-center">
+                {
+                    images ? images.map(image => <img alt={image} className="w-25 m-2" src={image} key={image}></img>) : ""
+                }
+            </div>
+            <div>
+                <video className="w-25 mt-5" src={json[1].videos[0]} preload="auto" controls></video>
+            </div>
+            {/* className="div-construccion" */}
+            {/* <h1 className="text-center">{primeraLetraMayuscula} realizados por BAVRV</h1>
+            <img src={fotoConstruccion[0].construccion} alt="construccion" width={"1000px"}></img> */}
         </div>
     )
 }
