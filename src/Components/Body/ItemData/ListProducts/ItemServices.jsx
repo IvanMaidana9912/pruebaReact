01984a477a91bca768414891d50f15732f3f38a7
Gaston Romero <107086703+GastonRomeroo@users.noreply.github.com>
2023-04-13 02:58:27 +0000

    realizando algunos cambios.

--- src/Components/Body/ItemData/ListProducts/ItemServices.jsx
+++ src/Components/Body/ItemData/ListProducts/ItemServices.jsx
@@ -1,22 +1,19 @@
 export const ItemServices = ({ send }) => {
     return (
         <div>
-
-            <div className="card mb-3" >
-                <div className="row g-0">
-                    <div className="col-md-4">
+            <div className="card mb-3 d-flex w-75" >
+                <div className="d-flex flex-row align-items-center">
+                    <div className="col">
                         {send.img ? <img src={send.img} key={send.img} className="img-fluid" alt={send.title} /> : ""}
                     </div>
-                    <div className="col-md-8">
+                    <div className="col">
                         <div className="card-body">
                             {send.title ? <h5 key={send.title}>{send.title}</h5> : ""}
-                            {send.detail ? <p key={send.detail}>{send.detail}</p> : ""}
+                            {send.detail ? <p className="text-start" key={send.detail}>{send.detail}</p> : ""}
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
     )
 }
--- src/Components/Body/ItemData/ListProducts/Services.jsx
+++ src/Components/Body/ItemData/ListProducts/Services.jsx
@@ -3,7 +3,10 @@ import { ItemServices } from "./ItemServices"
 export const Services = ({ items }) => {
     return (
         <div className="row my-5">
-            <div className="col-md-4 offset-md-3 text-center">
+            <div className="mb-2 text-center">
+                <h3>Nuestros servicios</h3>
+            </div>
+            <div className="col text-center">
                 {items.map(item => <ItemServices key={item.title} send={item} />)}
             </div>
         </div>
